f3e19eda39c7e954cb8d9bd9bf23cc8e3c531d4d
Matt Jewell <m@mattjewell.co.uk>
2021-10-08 11:41:14 +0200

    accepts and returns application/json

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  appStatus(): HelloResponse {
+    return { status: 'OK!' }; // because everything is always ok
   }
+}
+
+export interface HelloResponse {
+  status: string;
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +15,16 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/ (GET)', () => {
+    it('should be implemented consuming and producing “application/json”', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/')
+        .set('Accept', 'application/json');
+
+      expect(response).toHaveProperty(
+        'header.content-type',
+        expect.stringMatching(/^application\/json/),
+      );
+    });
   });
 });
